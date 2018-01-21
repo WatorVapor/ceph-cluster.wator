4285253a8a5f43bd5f03e47fadb4a694333181ff
Wator Vapor <watorvapor@gmail.com>
2018-01-22 00:38:18 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo ceph-mon -i node2.ceph.wator.xyz
-sudo ceph-osd -i 0 
-sudo ceph-osd -i 1 
-sudo ceph-osd -i 2 
+sudo ceph-osd -i 4 
+sudo ceph-osd -i 5 
+sudo ceph-osd -i 6 
 sudo ceph-mds -i node2.ceph.wator.xyz
