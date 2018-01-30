ec17d64e57c172a879fefb60a79fb67e9151c6dc
Wator Vapor <watorvapor@gmail.com>
2018-01-30 23:19:06 +0900

    Update osd.mount.sh

--- osd.mount.sh
+++ osd.mount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo mount -o user_xattr /dev/sda6 /ceph/osd-0
-sudo mount -o user_xattr /dev/sda7 /ceph/osd-1
-sudo mount -o user_xattr /dev/sda8 /ceph/osd-2
+sudo mount /dev/sda6 /ceph/osd-0
+sudo mount /dev/sda7 /ceph/osd-1
+sudo mount /dev/sda8 /ceph/osd-2
