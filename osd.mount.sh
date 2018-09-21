7ed93e060ba2e58208282946bf5cc4b939fee128
Wator Vapor <watovapor@gmail.com>
2018-09-21 16:02:00 +0000

    clean

--- osd.mount.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo mount /dev/sda6 /ceph/osd-0
-sudo mount /dev/sda7 /ceph/osd-1
-sudo mount /dev/sda8 /ceph/osd-2
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo ceph-mon -i node2.ceph.wator.xyz
-sudo ceph-mds -i node2.ceph.wator.xyz
